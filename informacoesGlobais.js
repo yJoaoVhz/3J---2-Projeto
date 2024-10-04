30eb12f1cbca2a3699562c85ba723897fc3d373b
yJoaoVhz <166820655+yJoaoVhz@users.noreply.github.com>
2024-10-04 09:43:53 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -11,7 +11,7 @@
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-texto');
 
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${totalPessoasMundo} de habitantes e destes ${totalPessoasConectadas} estão conectadas a alguma rede social e ficam em média ${tempoMedio} conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${totalPessoasMundo} </span> de habitantes e destes <span> ${totalPessoasConectadas} </span> estão conectadas a alguma rede social e ficam em média <span> ${tempoMedio} </span> conectadas.`
     secao.appendChild(paragrafo)
  }
 
